26c1cbb4ce95033b60a6d3b4445a8cdc10c6d056
mediDEU <mehdivasef@gmail.com>
2025-06-02 17:29:33 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.71, < 5.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0, < 4.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = “tfstate-gademo-rg”
+    storage_account_name = "tfstategademo"
+    container_name       = "tfstate"
+    key                  = "tfdemo.ga.tfstate"
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "13e3a757-2746-4a8a-8f2f-e6f3919648e7"
+}
+
+resource "azurerm_resource_group" "example" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
