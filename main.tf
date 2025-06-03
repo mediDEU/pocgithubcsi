51524a2533c1045478278734ed8b85f662b36dd1
mediDEU <mehdivasef@gmail.com>
2025-06-03 11:43:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,22 +4,3 @@ resource "azurerm_resource_group" "tfexample" {
   location = "West Europe"
 }
 
-# Create a Storage account
-resource "azurerm_storage_account" "terraform_state" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.tfexample.name
-  location                 = azurerm_resource_group.tfexample.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "my-terraform-env"
-  }
-}
-
-# Create a Storage container
-resource "azurerm_storage_container" "terraform_state" {
-  name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.terraform_state.name
-  container_access_type = "private"
-}
